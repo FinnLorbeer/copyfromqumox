0d11e496840d6fce8028ed9794cabda62a470d61
FinnVonFriesland <finn.von.friesland@googlemail.com>
2016-07-03 16:15:59 +0200

    Copy Images and Movies at once. Trailing zero for index in Filename.

--- copyfromqumox.rb
+++ copyfromqumox.rb
@@ -3,18 +3,29 @@ require 'fileutils'
 
 puts Dir['./*/*']
 
-index = 13
+def copy(filetype)
+  index = 1
 
-dir = Dir.glob('**/*.JPG').sort
+  dir = Dir.glob("**/*.#{filetype}").sort
 
-dir.each do |file|
-  f = file.to_s
-  puts "#{index}: copy #{f} ==> neuseeland_#{index}.JPG"
-  FileUtils.mv("#{f}", "./neuseeland_#{index}.JPG" )
-  4.times do
-    print '.'
-    sleep 1
-  end
-  index += 1
-  puts "done", ""
+  dir.each do |file|
+    f = file.to_s
+    filename = "segeln_2016_#{index.to_s.rjust(4, "0")}.#{filetype}"
+    puts "#{index}: copy #{f} ==> #{filename}"
+    FileUtils.mv("#{f}", "./#{filename}" )
+    10.times do
+      print '.'
+      sleep 0.1
+    end
+    index += 1
+    puts "done", ""
+  end 
+  
+  puts "all .#{filetype} copied!"
+
 end
+
+copy("JPG")
+copy("MOV")
+
+exit (0)
